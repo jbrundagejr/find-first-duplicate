ddcfc3a9fe8c578d6a81055667291f011493658a
Jon <jbrundagejr@gmail.com>
2021-08-06 10:50:02 -0400

    done

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,5 +1,19 @@
 function findFirstDuplicate(arr) {
-  // type your code here
+  // create two counters to compare
+  let first = 0
+  let second = 0
+  // check to see if array even has two numbers to compare. If not, exit
+  if (arr.length < 2) return -1
+  // iterate through array
+  for (let i = 0; i < arr.length; i++){
+    // make counters be concurrent elements of array
+    first = arr[i]
+    second = arr[i + 1]
+    // if elements are equal to each other, return first element found
+    if(arr[first] === arr[second]) return arr[i + 1]
+  }
+  // else return -1
+  return -1
 }
 
 if (require.main === module) {
